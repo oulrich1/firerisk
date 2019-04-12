d80729878ba11843f3e11531a6154f0702965d61
oulrich1 <oemulrich@gmail.com>
2019-04-12 16:16:39 -0700

    firerisk: zipcodes colored proportional to number of fires/total
    also added a slider for year selection to view properties of
    specific zipcode on specific year..
    
    TODO: add prediction parameters,
          add prediction parameter sliders
          add details to right hand empty space... (instead of tooltips)
          add prediction # years to details
          add % prob fire prediction as well (including weather data)

--- firerisk-d3viz.js
+++ firerisk-d3viz.js
@@ -1,21 +1,46 @@
 const log = console.log;
 log("Document Ready");
 
+function formatFireData(data) {
+  return {
+    fire_area_acres: +data.fire_area_acres,
+    fire_contained_date: data.fire_contained_date,
+    fire_name: data.fire_name,
+    fire_start_date: data.fire_start_date,
+    fire_year: +data.fire_year,
+    postal_code: data.postal_code,
+  }
+}
+
 async function getData() {
   const ca_counties = "./target-topojson/cb_2017_06_county_500k.topojson";
   const ca_zipcodes = "./target-topojson/cb_2017_06_zcta510_500k-simple.topojson";
   //const zip2county = "./d3-data/zcta_county_rel_10.csv";
   const zip2county = "./target-json/zcta_county_rel_10-06.json";
+  const postcalcode2firedata = "./common-data/pass_2/ca_postalcode_fire_intersections_data.csv";
+  // const openWeatherMapAPI = "https://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b6907d289e10d714a6e88b30761fae22";
   return await Promise.all([
     d3.json(ca_counties),
     d3.json(ca_zipcodes),
     d3.json(zip2county),
+    d3.csv(postcalcode2firedata, formatFireData),
+    // d3.json(openWeatherMapAPI, {crossOrigin: "anonymous", mode: 'no-cors'}),
   ])
 }
 
 async function main() {
-  const [ca_cou, ca_zips, glue] = await getData();
+  const [ca_cou, ca_zips, glue, fireHistory, weather] = await getData();
+  const minYear = d3.min(fireHistory, row => row.fire_year);
+  const maxYear = d3.max(fireHistory, row => row.fire_year);
+  let curFireHistory = fireHistory.filter(row => +row.fire_year === +minYear);
+  log(weather);
 
+  // fire_area_acres: "584.888061523437"
+  // fire_contained_date: "7/11/06"
+  // fire_name: "MESQUITE"
+  // fire_start_date: "7/9/06"
+  // fire_year: "2006"
+  // postal_code: "89019"
 
   /* These 'infos' maps are for tieing zips->counties */
   const zipInfos = d3.map();
@@ -33,121 +58,166 @@ async function main() {
     countyInfos.set(d.properties.COUNTYFP, d.properties);
   });
 
-  const body = d3.select('body');
-  const margin = {
-    left: 20,
-    right: 20,
-    top: 40,
-    bottom: 5,
-  };
-
+  const body = d3.select('body')
+  // const margin = { left: 20, right: 20, top: 40, bottom: 5 };
   const size = {
-    width: Math.min(1200, window.innerWidth || document.body.clientWidth),
-    height: Math.min(768, window.innerHeight || document.body.clientHeight),
+    width: Math.min(480, window.innerWidth || document.body.clientWidth),
+    height: Math.min(700, window.innerHeight || document.body.clientHeight),
   };
   const svg = body.append('svg')
     .attr('width', size.width)
     .attr('height', size.height);
 
-
-  var projection = d3.geoMercator()
-               .center([ -115.2, 37.5 ])
-               .translate([ size.width/2, size.height/2 ])
-               .scale([ size.width*3 ]);
+  const zipCodeColorScale = d3.scaleLinear()
+    .domain([0, curFireHistory.length])
+    .range(['#f9f9e3', 'darkorange']);
 
   const map = svg.append('g')
     .attr("class", "map")
     .attr("id", "TheMap");
 
-  /* These are for generating html for the Tooltips
-    * when hovering over Zipcode and County(if no zip exists
-    * at location in county) */
-  const getCountyHTML = (d) => {
-    const countyHTML = [ `County Name: ${d.properties.NAME}` ]
-      .map(v => `<span class='tip-li'>${v}</span>`)
-      .join('');
-    return `<div class='tooltip'> ${countyHTML} </div>`;
-  };
-  const tipCounty = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([-15, 0])
-    .html(getCountyHTML);
+  const projection = d3.geoMercator()
+    .center([-117, 37])
+    .translate([size.width / 1.5, size.height / 2])
+    .scale([size.width * 5]);
 
-  const getZipHTML = (d) => {
-    let props = d.properties;
-    const arr = zipInfos.get(props.ZCTA5CE10);
-    let zipCode = arr[0].ZCTA5;
-    let countyNames = arr
-      .map(el => countyInfos.get(el.COUNTY).NAME)
-      .join(', ');
-    const zipHTML = [ `County Name(s): ${countyNames}`, `ZipCode: ${zipCode}`]
-      .map(v => `<span class='tip-li'>${v}</span>`)
-      .join('');
-    return `<div class='tooltip'> ${zipHTML} </div>`;
-  };
+  drawCountyPaths();
+  drawZipCodePaths();
+  enableYearSlider()
+  enableZoom(svg, map);
 
-  const tipZip = d3.tip()
-    .attr('class', 'd3-tip')
-    .offset([-15, 0])
-    .html(getZipHTML);
+  // Title
+  svg
+    .append('text')
+    .attr('x', size.width / 2)
+    .attr('y', 30)
+    .style('text-anchor', 'middle')
+    .style('font-size', '2em')
+    .classed('title', true)
+    .text(`Fires in ${minYear}`);
 
-  /* This is where the data is drawn onto the SVG
-   * TODO: i think we should consider drawing with CANVAS
-   *       instead of SVG for performance reasons.
-   * */
-  const path = d3.geoPath().projection(projection);
-  const counties = map.append("g")
-      .attr("class", "counties");
-  counties.call(tipCounty);
-  counties.selectAll("path")
-    .data(topojson.feature(ca_cou, ca_cou.objects.counties).features)
-    .enter()
-    .append("path")
-      .attr('class', 'county-path')
-      .attr('id', (d) => {
-        return `${d.properties.COUNTYFP}`;
-      })
-      .attr("d", path)
-      .on('mouseover', tipCounty.show)
-      .on('mouseout', tipCounty.hide);
-
-
-  const zipPath = d3.geoPath().projection(projection);
-  const zipcodes = map.append("g")
-      .attr("class", "zipcodes");
-  zipcodes.call(tipZip);
-  zipcodes.selectAll("path")
-    .data(topojson.feature(ca_zips, ca_zips.objects.zipcodes).features)
-    .enter()
-    .append("path")
-      .attr('class', 'zipcode-path')
-      .attr("d", zipPath)
-    .on('mouseover', tipZip.show)
-    .on('mouseout', tipZip.hide);
-
-
-  /* This enables ZOOM on the map:
+  function enableZoom(svg, map) {
+    /* This enables ZOOM on the map:
     *  - scroll with mouse,
     *  - double click to zoom,
     *  - click and drag to PAN,
     *  - reset view with button */
-
-   const zoom = d3.zoom()
+    const zoom = d3.zoom()
       .scaleExtent([1, 5])
-   svg.call(zoom.on("zoom", () => {
-       map.attr("transform", d3.event.transform);
-     }));
- 
-   d3.select("button")
-     .on("click", () => {
-       svg.transition()
-         .duration(100)
-         .call(zoom.transform, d3.zoomIdentity);
-     });
+    svg.call(zoom.on("zoom", () => {
+      map.attr("transform", d3.event.transform);
+    }));
+    d3.select("#reset-zoom")
+      .on("click", () => {
+        svg.transition()
+          .duration(100)
+          .call(zoom.transform, d3.zoomIdentity);
+      });
+  }
 
+  function enableYearSlider() {
+    const sliderID = "#year-selection";
+    d3.select(sliderID)
+      .property("min", minYear)
+      .property("max", maxYear)
+      .property("value", minYear);
+    d3.select(sliderID)
+      .on("input", function () {
+        var year = +d3.event.target.value;
+        svg.select('.title')
+          .text(`Fires in ${year}`)
+        curFireHistory = fireHistory.filter(row => +row.fire_year === year);
+        d3.select('.zipcodes')
+          .selectAll("path")
+          .transition()
+          .attr('fill', d => {
+            const code = d.properties.ZCTA5CE10;
+            const fires = curFireHistory.filter(row => row.postal_code === code);
+            return zipCodeColorScale(fires.length);
+          });
+      });
+  }
+
+  function drawCountyPaths() {
+    /* These are for generating html for the Tooltips
+      * when hovering over Zipcode and County(if no zip exists
+      * at location in county) */
+    const getCountyHTML = (d) => {
+      const countyHTML = [ `County Name: ${d.properties.NAME}` ]
+        .map(v => `<span class='tip-li'>${v}</span>`)
+        .join('');
+      return `<div class='tooltip'> ${countyHTML} </div>`;
+    };
+    const tipCounty = d3.tip()
+      .attr('class', 'd3-tip')
+      .offset([-15, 0])
+      .html(getCountyHTML);
+
+    // TODO: i wonder if drawing with canvas or using
+    //       map tiles will be better performant
+    const path = d3.geoPath().projection(projection);
+    const counties = map.append("g")
+        .attr("class", "counties");
+    counties.call(tipCounty);
+    counties.selectAll("path")
+      .data(topojson.feature(ca_cou, ca_cou.objects.counties).features)
+      .enter()
+      .append("path")
+        .attr('class', 'county-path')
+        .attr('id', (d) => {
+          return `${d.properties.COUNTYFP}`;
+        })
+        .attr("d", path)
+        .on('mouseover', tipCounty.show)
+        .on('mouseout', tipCounty.hide);
+  }
+
+  function drawZipCodePaths() {
+    const getZipHTML = (d) => {
+      let props = d.properties;
+      const arr = zipInfos.get(props.ZCTA5CE10);
+      let zipCode = arr[0].ZCTA5;
+      let countyNames = arr
+        .map(el => countyInfos.get(el.COUNTY).NAME)
+        .join(', ');
+      const history = curFireHistory.filter(row => row.postal_code === zipCode);
+      const zipHTML = [
+          `County Name(s): ${countyNames}`,
+          `ZipCode: ${zipCode}`,
+          `# Fires: ${history.length} / ${curFireHistory.length}`]
+        .map(v => `<span class='tip-li'>${v}</span>`)
+        .join('');
+      return `<div class='tooltip'> ${zipHTML} </div>`;
+    };
+
+    const tipZip = d3.tip()
+      .attr('class', 'd3-tip')
+      .offset([-15, 0])
+      .html(getZipHTML);
+
+    const zipData = topojson.feature(ca_zips, ca_zips.objects.zipcodes).features;
+    const zipPath = d3.geoPath().projection(projection);
+    const zipcodes = map.append("g")
+        .attr("class", "zipcodes");
+    zipcodes.call(tipZip);
+    zipcodes.selectAll("path")
+      .data(zipData)
+      .enter()
+      .append("path")
+        .attr('class', 'zipcode-path')
+        .attr("d", zipPath)
+        .attr('fill', d => {
+          const code = d.properties.ZCTA5CE10;
+          const fires = curFireHistory.filter(row => row.postal_code === code);
+          return zipCodeColorScale(fires.length);
+        })
+      .on('mouseover', tipZip.show)
+      .on('mouseout', tipZip.hide);
+  }
 }
 
 main();
+
 
 
 // TODO: use tilemap for better performance, possibly..
