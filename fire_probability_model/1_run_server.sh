8729b1499990ef942921e39249b57c85bc711784
oulrich1 <oemulrich@gmail.com>
2019-04-18 15:16:57 -0700

    firerisk: fire risk prediction server

---
+++ fire_probability_model/1_run_server.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+# python server.py
+FLASK_APP=server.py flask run
--- fire_probability_model/server.py
+++ fire_probability_model/server.py
@@ -8,36 +8,41 @@ from xgboost import XGBClassifier
 
 from flask import Flask
 app = Flask(__name__)
+from flask import request
 
-filenames = glob('*.csv')
-data_list = [pd.read_csv(f) for f in filenames]
-df = pd.concat(data_list, axis=0, ignore_index=True)
-df = df.loc[:, ['elevation','PRCP','TMAX','TMIN','fire']]
-df.dropna(axis='rows', inplace = True)
-new_df = df.reset_index(drop=True)
+def testThatItWorks():
+    filenames =glob('./data/2001*.csv')
+    print(filenames)
+    data_list = [pd.read_csv(f) for f in filenames]
+    df = pd.concat(data_list, axis=0, ignore_index=True)
+    df = df.loc[:, ['elevation', 'PRCP', 'TMAX', 'TMIN', 'fire']]
+    df.dropna(axis='rows', inplace = True)
+    new_df = df.reset_index(drop=True)
 
-#Get class distribution
-print("No fire:", new_df['fire'][new_df['fire'] == 0].count()) #class = 0
-print("Fire:", new_df['fire'][new_df['fire'] == 1].count()) #class = 1
+    print("No fire:", new_df['fire'][new_df['fire'] == 0].count()) #class = 0
+    print("Fire:", new_df['fire'][new_df['fire'] == 1].count()) #class = 1
 
-# Split into training and testing sets
-X = new_df.drop('fire', axis=1)
-y = new_df['fire']
-X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size = 0.20, random_state=1)
+    X = new_df.drop('fire', axis=1)
+    y = new_df['fire']
+    X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size = 0.20, random_state=1)
+    print(X_test)
 
-#XGBoost default hyperparameters - Balanced Class Weights
-weights = (y==0).sum()/(1.0 *  (y==1).sum())
-xgb = XGBClassifier(random_state=1, scale_pos_weight = weights, n_jobs = -1)
-xgb.fit(X_train, y_train)
-xgb_pred = xgb.predict(X_test)
+xgb = XGBClassifier()
+xgb.load_model('./fire_probability.model')
 
-print("XGBoost - Testing Accuracy:", (round(accuracy_score(y_test, xgb_pred.round(0)), 2)))
-print('\n')
-print(confusion_matrix(y_test, xgb_pred))
-print('\n')
-print(classification_report(y_test, xgb_pred))
-print('\n')
-print("ROC AUC Score:", roc_auc_score(y_test,xgb_pred))
+def predict_prob(row):
+    df = pd.DataFrame(data=[row])
+    xgb_pred_prob = xgb.predict_proba(df) #Feed in API data here
+    return xgb_pred_prob
 
-#Predict Probability of being in Class 1
-xgb_pred_prob = xgb.predict_proba(X_test) #Feed in API data here
+@app.route("/predict", methods=['POST'])
+def predict():
+    req_data = request.get_json()
+    print(req_data)
+    elev = req_data['elevation']
+    prec = req_data['PRCP']
+    tmax = req_data['TMAX']
+    tmin = req_data['TMIN']
+    prob = predict_prob([float(elev), float(prec), float(tmax), float(tmin)])
+    return str(prob)
+
